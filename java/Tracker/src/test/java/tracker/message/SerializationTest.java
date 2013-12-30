caf3c3cc2edb9f7e747655d71365c46bb2361c6a
Ingo Jaeckel <ingo.jaeckel@googlemail.com>
2013-12-30 15:46:49 -0500

    added serialization test for long value

--- java/Tracker/src/test/java/tracker/message/SerializationTest.java
+++ java/Tracker/src/test/java/tracker/message/SerializationTest.java
@@ -1,9 +1,13 @@
 package tracker.message;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.util.Arrays;
+import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import tracker.common.Debugger;
 import tracker.common.ID;
+import tracker.common.Parser;
 import tracker.common.State;
 
 public class SerializationTest {
@@ -21,5 +25,18 @@ public class SerializationTest {
 	public void serializeUpdateOne() {
 		Debugger.print(new UpdateOneMessage(1, "abc", new State(0, 0).toString()).serialize());
 		Debugger.print(new UpdateOneMessage(1, ID.random().toString(), new State(0, 0).toString()).serialize());
+	}
+
+	@Test
+	public void serializeVersion() throws Exception {
+		Parser parser = new Parser();
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		DataOutputStream dos = new DataOutputStream(baos);
+		
+		dos.writeLong(384);
+		byte[] bytes = baos.toByteArray();
+		
+		Assert.assertTrue(Arrays.equals(bytes, new byte[] { 0, 0, 0, 0, 0, 0, 1, -128 }));
+		Assert.assertEquals(parser.readLong(bytes, 0), 384l);
 	}
 }
