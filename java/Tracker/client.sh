9ca7359c271c4435078beb74b8b473fced819480
Ingo Jaeckel <ingo.jaeckel@googlemail.com>
2014-01-02 23:19:08 -0600

    talk to localhost by default

--- java/Tracker/client.sh
+++ java/Tracker/client.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mvn exec:java -Dexec.mainClass="tracker.client.RunClient" -Dexec.args="192.168.1.104 6789 6000000"
+mvn exec:java -Dexec.mainClass="tracker.client.RunClient" -Dexec.args="127.0.0.1 6789 60000"
